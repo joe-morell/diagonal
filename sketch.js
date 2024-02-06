ed30c49db84d0a75caa0e4f184a92211518cab38
joe-morell <77936005+joe-morell@users.noreply.github.com>
2024-02-05 19:59:40 -0500

    first p5js app

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(400, 400);
+    x = 100
+    y = 100
+    dx = 1
+    dy = 1
+    
+  }
+  
+  function draw() {
+    background(220);
+    circle(x,y,100)
+    
+    x = x + dx
+    y = y + dy
+    
+    if (x > 300 || x < 100) {
+      dx = - dx
+    }
+    
+    if (y > 300 || y < 100) {
+      dy = - dy
+    }
+  
+  }
